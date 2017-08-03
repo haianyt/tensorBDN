62f7826b91e9464af98fe2f34c1544f697020ff3
tommy <haianyt@163.com>
2017-08-03 15:06:15 -0400

    fix bug

--- BDN.py
+++ BDN.py
@@ -100,8 +100,9 @@ class Model(ModelDesc):
 
                 #============================= from dorefa ======================================
                 l = activate(l)
-                l = Conv2D('conv1', l, in_channel, 1, stride=1, use_bias=False, nl=activate)
+                l = Conv2D('conv1', l, in_channel, 1, stride=1, use_bias=False)
                 l = fg(l)
+                l = activate(l)
                 #============================= from dorefa ======================================
 
                 # l = tf.nn.relu(l)
